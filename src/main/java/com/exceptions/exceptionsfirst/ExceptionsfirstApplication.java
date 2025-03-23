ca37480119c72051a18e24394231d3ba2479e8d6
Alvarado007 <samuel.alvarado@correounivalle.edu.co>
2025-03-23 14:01:17 -0500

    incializacion

---
+++ src/main/java/com/exceptions/exceptionsfirst/ExceptionsfirstApplication.java
@@ -0,0 +1,13 @@
+package com.exceptions.exceptionsfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionsfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionsfirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exceptions/exceptionsfirst/ExceptionsfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exceptions.exceptionsfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionsfirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
